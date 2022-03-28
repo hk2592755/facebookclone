28deeb663c330eb66e7ab25e0697f7221edef15e
hk2592755 <hk2592755@gmail.com>
2022-03-28 23:38:28 +0500

    relation

--- database/migrations/2022_03_28_144616_create_posts_table.php
+++ database/migrations/2022_03_28_144616_create_posts_table.php
@@ -15,7 +15,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('description');
+            $table->string('file_path');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
