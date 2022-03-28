a714c6a0a4b74792a34356111cc69bb5a257ba9c
hk2592755 <hk2592755@gmail.com>
2022-03-28 20:56:46 +0500

    second
    registration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,50 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/loginstyle.css')}}">
+    <title>Login</title>
+</head>
+<body>
 
-        <x-jet-validation-errors class="mb-4" />
+    <div class="box">
+        <div class="title-box">
+          <img src="https://i.postimg.cc/NMyj90t9/logo.png" alt="Facebook">
+          <p>Facebook helps you connect and share with the people in your life.</p>
+        </div>
+        <div class="form-box">
+            <x-jet-authentication-card>
+                <x-slot name="logo">
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+                </x-slot>
 
-        <form method="POST" action="{{ route('login') }}">
+                <x-jet-validation-errors class="mb-4" />
+                @if (session('status'))
+                <div class="mb-4 font-medium text-sm text-green-600">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+          <form method="POST" action="{{ route('login') }}">
             @csrf
+            <input type="text" name="email" placeholder="Email address or phone number">
+            <input type="password" name="password" placeholder="Password">
+            <button type="submit">
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+                {{ __('Log in') }}
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+            </button>
+            <a href="#">Forgotten Password</a>
+          </form>
+        </x-jet-authentication-card>
+          <hr>
+          <div class="create-btn">
+            <a href="{{route('welcome')}}" target="_blank">Create New Account</a>
+          </div>
+        </div>
+      </div>
+</body>
+</html>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
